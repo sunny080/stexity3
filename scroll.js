852ff8b8db8c12a87c888b1f196e79edf3793342
Sunny <77973332+sunny080@users.noreply.github.com>
2022-02-09 16:38:43 +0500

    Add files via upload

--- scroll.js
+++ scroll.js
@@ -54,3 +54,301 @@ window.addEventListener('scroll',()=>{
     }
 
 });
+
+
+
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.advise' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('acctive');
+
+    }else{
+        content.classList.remove('acctive')
+    }
+
+});
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.uic' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('apctive');
+
+    }else{
+        content.classList.remove('apctive')
+    }
+
+});
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.kil1' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('apctive');
+
+    }else{
+        content.classList.remove('apctive')
+    }
+
+});
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.kil2' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('apctive');
+
+    }else{
+        content.classList.remove('apctive')
+    }
+
+});
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.kil3' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('apctive');
+
+    }else{
+        content.classList.remove('apctive')
+    }
+
+});
+
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.kil4' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('apctive');
+
+    }else{
+        content.classList.remove('apctive')
+    }
+
+});
+
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.kil5' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('apctive');
+
+    }else{
+        content.classList.remove('apctive')
+    }
+
+});
+
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.kil6' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('apctive');
+
+    }else{
+        content.classList.remove('apctive')
+    }
+
+});
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.gip1' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('apctive');
+
+    }else{
+        content.classList.remove('apctive')
+    }
+
+});
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.center4' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('apctive');
+
+    }else{
+        content.classList.remove('apctive')
+    }
+
+});
+
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.alt1' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('apctive');
+
+    }else{
+        content.classList.remove('apctive')
+    }
+
+});
+
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.alt2' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('apctive');
+
+    }else{
+        content.classList.remove('apctive')
+    }
+
+});
+
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.alt3' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('apctive');
+
+    }else{
+        content.classList.remove('apctive')
+    }
+
+});
+
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.alt4' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('apctive');
+
+    }else{
+        content.classList.remove('apctive')
+    }
+
+});
+
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.alt5' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('apctive');
+
+    }else{
+        content.classList.remove('apctive')
+    }
+
+});
+
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.alt6' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('apctive');
+
+    }else{
+        content.classList.remove('apctive')
+    }
+
+});
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.alt7' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('apctive');
+
+    }else{
+        content.classList.remove('apctive')
+    }
+
+});
+
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.alt8' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('apctive');
+
+    }else{
+        content.classList.remove('apctive')
+    }
+
+});
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.alt9' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('apctive');
+
+    }else{
+        content.classList.remove('apctive')
+    }
+
+});
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.alt10' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('apctive');
+
+    }else{
+        content.classList.remove('apctive')
+    }
+
+});
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.alt11' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('apctive');
+
+    }else{
+        content.classList.remove('apctive')
+    }
+
+});
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.alt12' );
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight /1.5;
+    if(contentPosition < screenPosition){
+        content.classList.add('apctive');
+
+    }else{
+        content.classList.remove('apctive')
+    }
+
+});
